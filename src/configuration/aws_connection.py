19bb5c475e86a3786f9e6de2d626d7c99b0ddc99
krunal05 <krunalahir0538@gmail.com>
2025-12-28 16:52:21 +0530

    model evaluation and model pusher components added

---
+++ src/configuration/aws_connection.py
@@ -0,0 +1,35 @@
+import boto3
+import os
+from src.constants import AWS_SECRET_ACCESS_KEY_ENV_KEY, AWS_ACCESS_KEY_ID_ENV_KEY, REGION_NAME
+
+class S3Client:
+    s3_client = None
+    s3_resource = None
+    def __init__(self, region_name=REGION_NAME):
+        """
+        this class get aws credintials from env_variables and creates an connection with s3 bucket
+        and raises exception when environment variable is not set
+        """
+        if S3Client.s3_client is None or S3Client.s3_resource is None:
+            __access_key_id = os.getenv(AWS_ACCESS_KEY_ID_ENV_KEY)
+            __secret_access_key = os.getenv(AWS_SECRET_ACCESS_KEY_ENV_KEY)
+            if __access_key_id is None:
+                raise Exception(f"Environment variable {AWS_ACCESS_KEY_ID_ENV_KEY} not set")
+            if __secret_access_key is None:
+                raise Exception(f"Environment variable {AWS_SECRET_ACCESS_KEY_ENV_KEY} not set")
+
+            S3Client.s3_resource = boto3.resource('s3',
+                                                aws_access_key_id=__access_key_id,
+                                                aws_secret_access_key=__secret_access_key,
+                                                region_name=region_name
+                                                )
+
+            S3Client.s3_client = boto3.('s3',
+                                        aws_access_key_id=__access_key_id,
+                                        aws_secret_access_key=__secret_access_key,
+                                        region_name=region_name
+                                        )
+
+            self.s3_client = S3Client.s3_client
+            self.s3_resource = S3Client.s3_resource
+
